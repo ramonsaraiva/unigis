bd6bd94c76ce45da725e1219dcc08313902c88c1
Ramon Saraiva <ramonsaraiva@gmail.com>
2015-10-12 17:53:19 -0300

    fixed template, added libs/functions to run tornado sv as daemon

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from models import db
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
+import daemon
+
 app.config.update({
 	'SQLALCHEMY_DATABASE_URI': 'postgresql://x:x@localhost/x'
 })
@@ -32,3 +38,13 @@ def drop():
 @app.cli.command()
 def create():
 	db.create_all()
+
+@app.cli.command()
+def tornado():
+	log = open('tornado.log', 'a+')
+	ctx = daemon.DaemonContext(stdout=log, stderr=log, working_directory='.')
+	ctx.open()
+
+	http_server = HTTPServer(WSGIContainer(app))
+	http_server.listen(8080)
+	IOLoop.instance().start()
